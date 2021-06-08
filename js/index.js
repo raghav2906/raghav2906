6b0a66cd25df9eb536499cfbcbda53ef34286737
Raghav Agarwal <raghav.agarwalqw77@gmail.com>
2021-06-08 19:57:38 +0530

    corrects the scrolling defect

--- js/index.js
+++ js/index.js
@@ -22,7 +22,12 @@ function createSqaure(){
     square.style.width=20 + size + 'px';
     square.style.height=20 + size + 'px';
     square.style.top=Math.random() * innerHeight + 'px';
-    square.style.left = Math.random() * innerWidth + 'px';
+    var a
+    a = Math.random() * innerWidth ;
+    while(a >1400){
+        a=Math.random() * innerWidth;
+    }
+    square.style.left=a+'px';
     const bg = color[Math.floor(Math.random() * color.length)];
     square.style.background= bg;
     section.appendChild(square);
@@ -39,7 +44,12 @@ function createCircle(){
     circle.style.width=20 + size+ 'px';
     circle.style.height=20 + size+ 'px';
     circle.style.top=Math.random() * innerHeight + 'px';
-    circle.style.left = Math.random() * innerWidth + 'px';
+    var a;
+    a=Math.random() * innerWidth;
+    while(a>1000){
+        a=Math.random() * innerWidth;
+    }
+    circle.style.left = a + 'px';
     const bg = color[Math.floor(Math.random() * color.length)];
     circle.style.background= bg;
     sect.appendChild(circle);
